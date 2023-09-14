aeedc299adbe2451adc4dcc3b741f8d5a8e7c5db
pablo barra <pblo828@gmail.com>
2023-09-14 20:24:12 -0300

    commit pablo barra calculadoraaa

--- app/src/main/java/pablo/barra/calculadora/MainActivity.kt
+++ app/src/main/java/pablo/barra/calculadora/MainActivity.kt
@@ -15,58 +15,58 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pressDigit0(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 0
     }
 
     fun pressDigit1(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 1
     }
 
     fun pressDigit2(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 2
     }
 
     fun pressDigit3(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 3
     }
 
     fun pressDigit4(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 4
     }
 
     fun pressDigit5(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 5
     }
 
     fun pressDigit6(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 6
     }
 
     fun pressDigit7(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 7
     }
 
     fun pressDigit8(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 8
     }
 
     fun pressDigit9(view: View) {
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt2: TextView = findViewById(R.id.text2)
         txt2.text = txt2.text.toString() + 9
     }
 
     fun pressDigitSum(view: View) {
-        var text: TextView = findViewById(R.id.text)
-        var text2: TextView = findViewById(R.id.text2)
+        val text: TextView = findViewById(R.id.text)
+        val text2: TextView = findViewById(R.id.text2)
 
         total += text2.text.toString().toInt()
 
@@ -74,8 +74,8 @@ class MainActivity : AppCompatActivity() {
         text2.text = ""
     }
     fun pressDigitEqual(view: View) {
-        var txt1: TextView = findViewById(R.id.text)
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt1: TextView = findViewById(R.id.text)
+        val txt2: TextView = findViewById(R.id.text2)
 
         val operand2 = txt2.text.toString().toInt()
 
@@ -91,8 +91,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pressDigitC(view: View) {
-        var txt: TextView = findViewById(R.id.text)
-        var text2: TextView = findViewById(R.id.text2)
+        val txt: TextView = findViewById(R.id.text)
+        val text2: TextView = findViewById(R.id.text2)
 
         total = 0
 
@@ -101,8 +101,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pressDigitpunto(view: View) {
-        var txt1: TextView = findViewById(R.id.text)
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt1: TextView = findViewById(R.id.text)
+        val txt2: TextView = findViewById(R.id.text2)
 
         total = txt2.text.toString().toInt()
 
@@ -111,8 +111,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pressDigitmenos(view: View) {
-        var txt1: TextView = findViewById(R.id.text)
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt1: TextView = findViewById(R.id.text)
+        val txt2: TextView = findViewById(R.id.text2)
 
         total -= txt2.text.toString().toInt()
 
@@ -121,18 +121,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pressDigitPorcentajes(view: View) {
-        var txt1: TextView = findViewById(R.id.text)
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt1: TextView = findViewById(R.id.text)
+        val txt2: TextView = findViewById(R.id.text2)
 
-        total %= txt2.text.toString().toInt()
+        val operand2 = txt2.text.toString().toInt()
+        total = (total * operand2) / 100
 
         txt1.text = txt1.text.toString() + txt2.text.toString() + "%"
         txt2.text = ""
+
     }
 
     fun pressDigitParentesis(view: View) {
-        var txt1: TextView = findViewById(R.id.text)
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt1: TextView = findViewById(R.id.text)
+        val txt2: TextView = findViewById(R.id.text2)
 
         total = txt2.text.toString().toInt()
 
@@ -141,8 +143,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pressDigitdivicion(view: View) {
-        var txt1: TextView = findViewById(R.id.text)
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt1: TextView = findViewById(R.id.text)
+        val txt2: TextView = findViewById(R.id.text2)
 
         total /= txt2.text.toString().toInt()
 
@@ -152,10 +154,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pressDigitPor(view: View) {
-        var txt1: TextView = findViewById(R.id.text)
-        var txt2: TextView = findViewById(R.id.text2)
+        val txt1: TextView = findViewById(R.id.text)
+        val txt2: TextView = findViewById(R.id.text2)
 
-        total *= txt2.text.toString().toInt()
+        val operand2 = txt2.text.toString().toInt()
+        total *= operand2
 
         txt1.text = txt1.text.toString() + txt2.text.toString() + "*"
         txt2.text = ""
